339802e710a0e796d2d1deebbb7be0a57cf57aae
Daniel Akproh <daniel.akproh@outlook.com>
2023-12-26 22:25:07 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Intro from './Components/Intro'
+import Background from './Components/Background'
+import Contact from './Components/Contact'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Intro />
+      <Background />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
