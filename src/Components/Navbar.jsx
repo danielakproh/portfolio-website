e785868b55c2c039bae7be810b0eafcb25ddff53
Daniel Akproh <daniel.akproh@outlook.com>
2023-12-27 14:08:13 -0500

    define navbar skeleton

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <header>
+            <nav className='flex justify-between items-center'>
+                <div className='logo'>
+                    <img src="" alt="" />
+                    <a href="">Brand</a>
+                </div>
+                <div>
+                    <a href="" className='mr-10'>Home</a>
+                    <a href="" className='mr-10'>Experience</a>
+                    <a href="" className='mr-10'>Projects</a>
+                    <a href="" className='mr-10'>About</a>
+                    <a href="" className='mr-10'>Contact</a>
+                </div>
+            </nav>
+        </header>
     )
 }
 
