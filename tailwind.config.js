87cf53f1bd8d88cc5855ff069e668a5b835942cf
Daniel Akproh <daniel.akproh@outlook.com>
2024-01-07 01:49:37 -0500

    implement basic layout styles for navbar and intro sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,15 @@ export default {
     extend: {
       // defining custom colors. See
       colors: {
-        "peace-brown": '#F1EAE1',
-        "bright-green": '#AEBAA1'
+        "beige-background": '#FFE6CB',
+        "bright-green": '#AEBAA1',
+        "light-grey": '#6E6A6A', // navbar elements, job title
+        "emphasis-orange": '#D19B66', // buttons, words of emphasis and section titles underline
+        "ghost-grey": '#A7A7A7', // underlay texts: Background, Projects, About, contact. alpha = 19%
+        "milky-brown": '#DFC3A5', // job cards, project cards & footer
+        "dark-grey": '#A7A7A7', // timeline bar. alpha value = 100%
+        "brown-chocolat": '#605555', // timeline dates
+        "hr-separator-grey": 'rgba(255,173,0,0.36)' // alpha = 35%
       },
     },
   },
