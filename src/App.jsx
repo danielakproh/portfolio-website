f9935ae075276697642eb177f1b1e060e9ad6f95
Daniel Akproh <daniel.akproh@outlook.com>
2024-01-07 01:42:27 -0500

    specify custom design rules and color palettes based on figma design

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
 import React from 'react'
-import Navbar from './Components/Navbar'
-import Intro from './Components/Intro'
-import Background from './Components/Background'
-import Projects from './Components/Projects'
-import About from './Components/About'
-import Contact from './Components/Contact'
-import Footer from './Components/Footer'
+
+import Title from './Components/common/Title'
+
+import Navbar from './Components/pages/Navbar'
+import Intro from './Components/pages/Intro'
+import Background from './Components/pages/Background'
+import Projects from './Components/pages/Projects'
+import About from './Components/pages/About'
+import Contact from './Components/pages/Contact'
+import Footer from './Components/pages/Footer'
 
 import './index.css'
 
 const App = () => {
   return (
-    <div className='mx-96 mt-2'>
-      <Navbar />
-      <Intro />
-      <Background />
-      <Projects />
-      <About />
-      <Contact />
-      <Footer />
-
-      <div className="card">blahblah</div>
-    </div>
+    <>
+      <div className='mx-96'>
+        <Navbar />
+        <Intro />
+        {/* ******************************************************* */}
+        <Title frontText="My Journey" backgroundText="Background" />
+        <Background />
+        {/* ******************************************************* */}
+        <Title frontText="My Works" backgroundText="Projects" />
+        <Projects />
+        {/* ******************************************************* */}
+        <Title frontText="Know me more" backgroundText="About" />
+        <About />
+        {/* ******************************************************* */}
+        <Title frontText="Get in Touch" backgroundText="Contact" />
+        <Contact />
+        <Footer />
+      </div>
+    </>
   )
 }
 
